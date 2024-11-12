e3ac5f52537929cb7498df8fe67ac5bbe5bbd1e4
Gianluca Bellocchi <gbellocchi95@unimore.it>
2024-11-12 19:02:22 +0100

    Modification to environment script for vitis hls vision library

--- accelerators/env/vitis_hls_vision_library.sh
+++ accelerators/env/vitis_hls_vision_library.sh
@@ -1,24 +1,40 @@
-# Copyright (C) 2023 University of Modena and Reggio Emilia.
+#!/bin/bash
+# Copyright (C) 2024 University of Modena and Reggio Emilia.
 # Author: Gianluca Bellocchi, University of Modena and Reggio Emilia.
 
 # Paths
 workspace_vitis_vision=$(realpath $(dirname "$(realpath ${BASH_SOURCE:-$0})")/..)
+env_path=$workspace_vitis_vision/env
 deps_path=$workspace_vitis_vision/deps
 tools_dir=$workspace_vitis_vision/tools
 vitis_path=$workspace_vitis_vision/deps/Vitis_Libraries
 
-# Install paths
-vitis_install_path= # insert the path to your Vitis build directory, e.g., `some/path/Vitis/2022.2`.
-opencv_install_path= # insert the path to your OpenCV directory, e.g., `some/path/opencv`.
+# Environment check
+source $env_path/check_environment.sh
+if is_empty_or_undefined $vitis_install_path; then
+    echo "Define <vitis_install_path>."
+    echo "Check the Richie documentation for more information: https://richie-docs.readthedocs.io/en/latest/."
+    must_be_nonempty vitis_install_path ""
+fi
+if is_empty_or_undefined $opencv_install_path; then
+    echo "Define <opencv_install_path>."
+    echo "Check the Richie documentation for more information: https://richie-docs.readthedocs.io/en/latest/."
+    must_be_nonempty opencv_install_path ""
+fi
 
-# AMD-Xilinx tools
-export TOOL_VERSION=2022.2 # Important for Vitis HLS in this case
-export VIVADO="vivado"
-export VIVADO_HLS="vivado_hls"
-export VITIS_HLS="vitis_hls"
-export PETALINUX_VER="" 
+# Install setup
+if [ $UNIMORE -eq 1 ]; then
+
+    export VITIS_HLS="vitis_hls"
+
+elif [ $IIS -eq 1 ]; then
+
+  export VITIS_HLS="vitis-2022.1 vivado_hls"
+
+fi
 
 # AMD-Xilinx setup
+export TOOL_VERSION=2022.2
 source $vitis_install_path/settings64.sh
 export DEVICE=$vitis_install_path/base_platforms/xilinx_zcu102_base_202220_1/xilinx_zcu102_base_202220_1.xpfm
 export PLATFORM_REPO_PATHS=$vitis_install_path/base_platforms/xilinx_zcu102_base_202220_1
